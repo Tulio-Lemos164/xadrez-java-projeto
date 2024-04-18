1e4c1ca480f87b97bad0da819887004742a0d3ab
Tulio Lemos <tlcbrasil164@gmail.com>
2024-04-18 19:50:41 -0300

    Começando os movimentos especificos das peças

--- src/application/UI.java
+++ src/application/UI.java
@@ -29,6 +29,11 @@ public class UI {
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+    //Esse código só roda no Git Bash, não no console
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
 
     public static ChessPosition readChessPosition(Scanner sc){
         try{
