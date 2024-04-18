68b2d41bd5177d04728891286fc571891ff8cedb
Tulio Lemos <tlcbrasil164@gmail.com>
2024-04-18 15:23:37 -0300

    Camada chess e logica inicial (printar tabuleiro e peças)

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,9 +1,13 @@
 package application;
 
+import chess.ChessMatch;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Xadrez!");
+
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
     }
 }
