bf2f5c34abbf5c1dd9916c363a45debf9ff26710
Tulio Lemos <tlcbrasil164@gmail.com>
2024-04-18 19:18:37 -0300

    Movendo as peças no tabuleiro!

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,13 +1,31 @@
 package application;
 
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
-        UI.printBoard(chessMatch.getPieces());
+
+        while (true){
+            UI.printBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.println("Posição de inicio:");
+            ChessPosition source = UI.readChessPosition(sc);
+
+            System.out.println();
+            System.out.println("Posição de final");
+            ChessPosition target = UI.readChessPosition(sc);
+
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+        }
+
     }
 }
